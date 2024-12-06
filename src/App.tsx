7ab8cf64c8fc38e02a2dda5db1a573330e8820cb
Carlos563429 <carlitosfg.5051@gmail.com>
2024-12-06 15:03:15 -0600

    [🦈] Feature: Finish Static page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Card from './components/card.tsx';
 import ProfileCard from './components/ProfileCard.tsx';
 import SkillCard from './components/SkillCard.tsx';
 import SocialCard from './components/SocialCard.tsx';
+import ProjectCard from './components/ProjectCard.tsx';
 import data from "./assets/data.json"
 
 function InfoBody(){
@@ -11,7 +12,8 @@ function InfoBody(){
             < ProfileCard title={data.name} subtitle='A software developer' avatarUrl='/avatar.webp'/>
             < Card title='About me' subtitle='' content={data['about-me']}/>
             < SkillCard title='My skills' data={data['skill-list']}/>
-            < SocialCard title='My socials' data={data['social-media']}/>
+            < ProjectCard title='My project' data={data['project-list']} />
+             < SocialCard title='My socials' data={data['social-media']}/>
         </div>)
 }
 
