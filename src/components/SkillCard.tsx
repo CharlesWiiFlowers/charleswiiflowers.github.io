d37f6891025062aa7568c885e5cc614450d26805
Carlos563429 <carlitosfg.5051@gmail.com>
2024-12-18 13:02:30 -0600

    [🦈] Commit: Add format

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -1,38 +1,38 @@
 import Card from './card.tsx';
 
-function SkillCard({title, data}:
-        {title: string, data: Array<object>}){
+function SkillCard({ title, data }:
+    { title: string, data: Array<object> }) {
 
-        let language_row: string[] = [];
-        let techology_row: string[] = [];
+    const language_row: string[] = [];
+    const techology_row: string[] = [];
 
-        data.forEach(element => {
-            if(element.category == "languages"){language_row.push(element.source)}
-            if(element.category == "technologies"){techology_row.push(element.source)}
-        });
+    data.forEach(element => {
+        if (element.category == "languages") { language_row.push(element.source) }
+        if (element.category == "technologies") { techology_row.push(element.source) }
+    });
 
-        return (
-            <Card title={title} subtitle='' content=''>
-                <div className='div-languages'>
-                    <h2>Languages</h2>
-                    <div className='div-image'>
-                        {language_row.map((src, index) => (
-                            <img key={index} src={src} className='skill-image'/>
-                        ))}
-                    </div>
+    return (
+        <Card title={title} subtitle='' content=''>
+            <div className='div-languages'>
+                <h2>Languages</h2>
+                <div className='div-image'>
+                    {language_row.map((src, index) => (
+                        <img key={index} src={src} className='skill-image' />
+                    ))}
                 </div>
-                <div className='div-technologies'>
-                    <h2>Technologies</h2>
-                    <div className='div-image'>
-                        {techology_row.map((src, index) => (
-                            <div className={'div' + index.toString()}>
-                                <img key={index} src={src} className='skill-image'/>
-                            </div>
-                        ))}
-                    </div>
+            </div>
+            <div className='div-technologies'>
+                <h2>Technologies</h2>
+                <div className='div-image'>
+                    {techology_row.map((src, index) => (
+                        <div className={'div' + index.toString()}>
+                            <img key={index} src={src} className='skill-image' />
+                        </div>
+                    ))}
                 </div>
-            </Card>
-        )
-    }
+            </div>
+        </Card>
+    )
+}
 
 export default SkillCard
